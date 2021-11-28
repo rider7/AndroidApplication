311cc6a3b1e524987eaee350fb3cce77bad56da2
rider7 <brandon.rider@gmail.com>
2021-11-28 00:33:15 -0600

    commitcommit
    termeditnotcomplete

---
+++ app/src/main/java/br/androidapplication/UserInterface/CourseActivity.java
@@ -0,0 +1,21 @@
+package br.androidapplication.UserInterface;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CourseActivity  extends AppCompatActivity {
+
+
+
+
+
+
+
+
+
+    public void addCourseFromScreen(View view) {
+
+    }
+
+}
